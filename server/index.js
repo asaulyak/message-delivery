0021e792431da86c2364eb38ecedfc3f422273a4
Eugene Asaulyak <jenya.asaulyak@gmail.com>
2018-01-05 12:53:43 +0200

    Add server
    Added basic server that can put one message ro the queue

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const amqp = require('amqplib');
+const config = require('../config');
+
+amqp.connect(config.rabbitMQ.connection)
+  .then(connection => {
+    return connection.createChannel()
+      .then(channel => channel.assertQueue(config.rabbitMQ.queue, {durable: true})
+        .then(() => channel))
+      .then(channel => {
+        const msg = process.argv.slice(2).join(' ') || 'Hello World!';
+        channel.sendToQueue(config.rabbitMQ.queue, Buffer.from(msg), {
+          deliveryMode: true
+        });
+        console.log(' [x] Sent \'%s\'', msg);
+        return channel.close();
+      })
+      .finally(() => {
+        connection.close();
+      })
+  })
+  .catch(console.warn);
